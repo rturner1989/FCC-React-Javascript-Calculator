210254e9f562b75f77a083fa75423719aebc1922
rturner1989 <robturner1000@hotmail.co.uk>
2021-07-14 14:47:27 +0100

    Functionality Update
    - Seperated events into new functions

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,82 +1,92 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import Keypad from "./Keypad";
 import Display from "./Display";
 import * as math from "mathjs";
-import { string } from "mathjs";
 
 const Calculator = () => {
     const [equationDisplay, setEquationDisplay] = useState("");
-    const [calculatedResult, setCalculatedResult] = useState("0")
-    const [equals, setEquals] = useState(null)
-    
+    const [calculatedResult, setCalculatedResult] = useState("0");
+    const [equals, setEquals] = useState(null);
 
-    const handleClick = (value)=>{
-        const numbers = new RegExp(/[0-9]/);
-        const operators = new RegExp(/[-,+,*,\/]/);
-        const decimal = new RegExp(/[.]/);
-        // if value matches a number
-        if(numbers.test(value)){
-            if(equals !== null){
-                setEquals(null);
-                setCalculatedResult("");
-                setEquationDisplay("");
-            }
-            if(calculatedResult.length === 1 && calculatedResult.charAt(0) === "0"){
-                setCalculatedResult(value);
-            } else {
-                setCalculatedResult((val)=> val += value);
-            }
-            if(operators.test(calculatedResult)){
-                setCalculatedResult(value);
-            } 
-            
-            setEquationDisplay((val)=> val += value);
-        } 
-        // if value matches an operator 
-        else if (operators.test(value)){
-            if(equals !== null){
-                setEquationDisplay(`${equals}${value}`);
-                setCalculatedResult(value);
-                setEquals(null);
-            } 
-            else if (operators.test(calculatedResult) && value !== "-"){
-                if(calculatedResult === "-"){
-                    setCalculatedResult(value);
-                    setEquationDisplay((val)=> val.substr(0, val.length -2)+ value);
-                } else {
-                    setCalculatedResult(value);
-                    setEquationDisplay((val)=> val.substr(0, val.length -1)+ value);
-                }
-            } else {
-                    setCalculatedResult(value);
-                    setEquationDisplay((val)=> val += value);
+    const isOperator = new RegExp(/[-,+,*,\/]/);
+
+    const handleClick = (value) => {
+        if (value === ".") {
+            if (!calculatedResult.includes(value)) {
+                setEquationDisplay((val) => (val += value));
+                setCalculatedResult((val) => (val += value));
             }
         }
-        // if value matches a decimal
-        else if(decimal.test(value)){
-            if(!calculatedResult.includes(value)){
-                setEquationDisplay((val) => val += value);
-                setCalculatedResult((val) => val += value);
-            } 
+    };
+
+    const handleNumbers = (value) => {
+        if (equals !== null) {
+            setEquals(null);
+            setCalculatedResult("");
+            setEquationDisplay("");
         }
-        
-    }
-    const calculate = ()=>{
-        const result = math.evaluate(equationDisplay);
-        setCalculatedResult(result);
-        setEquationDisplay((val)=> val += `=${result}`)
-        setEquals(result)
-    }
-    const clearInput = ()=>{
-        setEquationDisplay("")
-        setCalculatedResult("0")
-    }
-    
+        if (
+            calculatedResult.length === 1 &&
+            calculatedResult.charAt(0) === "0"
+        ) {
+            setCalculatedResult(value);
+        } else {
+            setCalculatedResult((val) => (val += value));
+        }
+        if (isOperator.test(calculatedResult)) {
+            setCalculatedResult(value);
+        }
+        setEquationDisplay((val) => (val += value));
+    };
+
+    const handleOperators = (value) => {
+        if (equals !== null) {
+            setEquationDisplay(`${equals}${value}`);
+            setEquals(null);
+        } else if (value === "-") {
+            setEquationDisplay((val) => (val += value));
+        } else if (isOperator.test(calculatedResult)) {
+            if (calculatedResult === "-") {
+                setEquationDisplay(
+                    (val) => val.substr(0, val.length - 2) + value
+                );
+            } else {
+                setEquationDisplay(
+                    (val) => val.substr(0, val.length - 1) + value
+                );
+            }
+        } else {
+            setEquationDisplay((val) => (val += value));
+        }
+        setCalculatedResult(value);
+    };
+
+    const calculate = () => {
+        if (equals === null) {
+            const result = math.evaluate(equationDisplay);
+            setCalculatedResult(result);
+            setEquationDisplay((val) => (val += `=${result}`));
+            setEquals(result);
+        }
+    };
+    const clearInput = () => {
+        setEquationDisplay("");
+        setCalculatedResult("0");
+    };
 
     return (
         <div id="calculator">
-            <Display  equationDisplay={equationDisplay} calculatedResult={calculatedResult} />
-            <Keypad  handleClick={handleClick} clearInput={clearInput} calculate={calculate}/>
+            <Display
+                equationDisplay={equationDisplay}
+                calculatedResult={calculatedResult}
+            />
+            <Keypad
+                handleClick={handleClick}
+                handleNumbers={handleNumbers}
+                handleOperators={handleOperators}
+                clearInput={clearInput}
+                calculate={calculate}
+            />
         </div>
     );
 };
--- src/Display.js
+++ src/Display.js
@@ -1,10 +1,10 @@
 import React from "react";
 
-const Display = ({ equationDisplay, calculatedResult}) => {
+const Display = ({ equationDisplay, calculatedResult }) => {
     return (
         <section className="display">
             <h3 id="input">{equationDisplay}</h3>
-            <h1 id="display" >{calculatedResult}</h1>
+            <h1 id="display">{calculatedResult}</h1>
         </section>
     );
 };
