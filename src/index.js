80bddf706bac8acf148733b5acd0f8b2b33d94b9
rturner1989 <robturner1000@hotmail.co.uk>
2021-08-18 17:39:04 +0100

    fixed layout bug
    - added bootstrap package

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import "./Styles/index.css";
 import App from "./App";
 import { AppProvider } from "./Context";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(
     <React.StrictMode>
